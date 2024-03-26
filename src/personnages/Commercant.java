ce5b002a5a75ee875a623a9d6b57b298e8d4e4be
Lubin Boscredon <bsl4971a@ensfsi.onmicrosoft.com>
2024-03-26 19:55:00 +0100

    fin TP

---
+++ src/personnages/Commercant.java
@@ -0,0 +1,18 @@
+package personnages;
+
+public class Commercant extends Humain {
+
+	public Commercant(String nom, int argent) {
+		super(nom, "Thé", argent);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int seFaireExtorquer(perte) {
+		
+	}
+	
+	public void recevoir(int argent) {
+		parler(argent + " sous ! Je te remercie généreux donateur!");
+	}
+	
+}
